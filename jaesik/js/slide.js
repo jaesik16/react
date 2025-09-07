2463f77407f4bb72363c68b93030095e12e15dd0
jaesik16 <qweqwexz@naver.com>
2025-09-07 17:01:13 +0900

    깃 허브 연습

---
+++ jaesik/js/slide.js
@@ -0,0 +1,88 @@
+(function($){  // 매개변수(파라미터 Parameter)
+    const obj = {  // 객체 리터럴 방식 권장
+        init(){  // 대표 메서드
+            this.header();
+            this.section1();
+            this.section2();
+            this.section3();
+        },
+        header(){},
+        section1(){
+            let cnt=0;
+            let setId=0;
+            const slideWrap = $('.slide-wrap');
+            const pageBtn = $('.page-btn');
+            const stopBtn = $('.stop-btn');
+            const playBtn = $('.play-btn');
+            const n = ($('.slide').length-2)-1;
+
+            // 1. 메인슬라이드함수
+            function mainSlide(){
+                slideWrap.stop().animate({left: `${-100*cnt}%`}, 600, 'easeInOutExpo', function(){
+                    if(cnt>n){cnt=0}
+                    if(cnt<0){cnt=n}
+                    slideWrap.stop().animate({left: `${-100*cnt}%`}, 0);
+                });
+                pageEvent();
+            }
+
+            // 2. 다음카운트함수
+            function nextCount(){
+                cnt++;
+                mainSlide();
+            }
+
+            // 3. 자동타이머함수(7초 후 7초간격 계속)
+            function autoTimer(){
+                setId = setInterval(nextCount, 7000);
+            }
+            autoTimer();
+
+            // 4. 페이지 이벤트 함수
+            function pageEvent(){
+                pageBtn.removeClass('on');
+                pageBtn.eq( cnt>n ? 0 : cnt).addClass('on');
+            }
+
+            // 5. 페이지버튼클릭
+            pageBtn.each(function(idx){
+                $(this).on({
+                    click(e){
+                        e.preventDefault();
+                        cnt=idx;
+                        mainSlide();
+                        clearInterval(setId); // 클릭시 일시중지
+                    }
+                });
+            });
+
+            // 6-1. 스톱 버튼 클릭이벤트
+            stopBtn.on({
+                click(e){
+                    e.preventDefault();
+                    stopBtn.addClass('on');
+                    playBtn.addClass('on');
+                    clearInterval(setId); // 클릭시 일시중지
+                }
+            })
+
+            // 6-2. 플레이 버튼 클릭이벤트
+            playBtn.on({
+                click(e){
+                    e.preventDefault();
+                    stopBtn.removeClass('on');
+                    playBtn.removeClass('on');
+                    autoTimer(); // 클릭시 재실행 7초후실행
+                }
+            })
+
+            
+        },
+        section2(){
+            
+        },
+        section3(){},
+    }
+    obj.init();
+
+})(jQuery); // 전달인수(아규먼트 Argument)
